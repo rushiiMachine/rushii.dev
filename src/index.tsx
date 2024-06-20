0cf434a14f1f1eda16b6008231706ffe96b0091b
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2024-06-20 14:04:21 -0700

    feat: styled overlay scrollbar

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,18 @@ import { render } from 'solid-js/web';
 import App from './App';
 import './index.css';
 
+import 'overlayscrollbars/overlayscrollbars.css';
+import { OverlayScrollbars } from 'overlayscrollbars';
+
+// `overflow: overlay` got deprecated & removed, so it's now impossible to get native scroll bars to overlay content!!!
+// I sure love using giant JS libraries to fix what the browser should already fucking do!
+OverlayScrollbars(document.querySelector('body')!, {
+    overflow: {
+        x: 'hidden',
+        y: 'scroll',
+    },
+});
+
 render(() => <App/>, document.getElementById('root')!);
 
 // Fuck Dark Reader, it ignores the meta tag
